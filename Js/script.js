3fb69ff37aaf88ea2f772f590562354b233e460f
Oluwagbemiga <samuelifeoluwa540@gmail.com>
2023-04-10 10:40:40 +0200

    stash changes

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,32 @@
+// BEFORE USING FIREBASE SERVICES, THE FIREBASE APP MUST BE INITIALIZED FIRST, BECAUSE WITHOUT IT, YOU CAN'T GET ACCESS TO THE FIREBASE LIBRARY FROM THE NET. THIS IS WHY IT'S IMPORTANT TO ALWAYS IMPORT THE FIREBASE APP, IMMEDIATELY AFTER INSTALLING FIREBASE, AND BEFORE YOU USE IT ON ANY OF YOUR PROJECTS.
+
+// IMPORT THE SERVICE BEFORE CALLING IT.
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+// import {get} from
+
+const firebaseApp = initializeApp({});
+
+const auth = getAuth(firebaseApp);
+
+// monitor authentication state.
+onAuthStateChanged(auth, (user) => {
+  if (user != null) {
+    alert("LOGGED IN");
+  } else {
+    alert("Not Logged In");
+  }
+});
+
+// What treeshaking does.
+auth.onAuthStateChanged((user) => {
+  if (user != null) {
+    console.log("Logged In");
+  } else {
+    console.log("Not logged in, and no user");
+  }
+});
+
 const heroFormInput = document.querySelector(".hero__");
 const notifyFormInput = document.querySelector(".noti__");
 
---
+++ src/firebase.js
@@ -0,0 +1,5 @@
+// FOR THE FIREBASE BACKEND OF THE WEBSITE.
+import { initializeApp } from "firebase";
+import { getAuth } from "firebase/auth";
+
+// const firebaseApp =
