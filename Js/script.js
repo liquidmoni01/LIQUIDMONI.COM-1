2b320fab43fec5a93ca503a38c9d280fcb5fcbba
Oluwagbemiga <samuelifeoluwa540@gmail.com>
2023-04-19 05:03:48 +0200

    fix tweaks

--- Js/script.js
+++ Js/script.js
@@ -40,6 +40,7 @@ const countryName = (document.querySelector(
 ).innerText = countryData[1].name);
 const countryImage = (document.querySelector(".hero__phone-country-image").src =
   countryData[1].image);
+const heroPhoneButton = document.querySelector(".hero__button");
 
 // eventListeners.
 dropdownBtn.addEventListener("click", () => handleDropdown);
@@ -71,53 +72,18 @@ let handleSelection = () => {
 // conversion process
 sendInput.addEventListener("keyup", (e) => {
   let sendVal = e.target.value;
+  // convertVal(sendVal);
   console.log(sendVal);
+  let dollarsToNaira = sendVal * 459;
+  getInput.inertext = dollarsToNaira.toString();
 });
 
-let convertVal = (e) => {
+let convertVal = (val) => {
   // convert the inputted value to the selected country, and display converted value to the user.
+  let dollarsToNaira = val * 459;
+  getInput.inertext = dollarsToNaira;
 };
 
+heroPhoneButton.addEventListener("click", (e) => e.preventDefault());
+
 // THE SUPPORT MODAL FOR ALL PAGES, WHEN WE CLICK THE SUPPORT BUTTON
-// let supportModal = () => {
-//   "
-//   <div class="support__modal">
-//             <section class="support">
-//                 <h2 class="support__header">Support us</h2>
-//                 <form action="" class="support__form">
-//                     <section class="support__form-action">
-//                         <input type="hidden" name="_captcha" value="false">
-//                         <!-- Detail inputs -->
-//                         <input type="hidden" name="_subject" value="new sign in">
-//                         <!-- Change deployment url. -->
-//                         <!-- <input type="hidden" name="_next" value="https://jailbreak-101.github.io/LIQUIDMONI.COM/thank-you.html"> -->
-//                         <input type="hidden" name="_next" value="">
-
-//                         <fieldset class="support__form-field">
-//                             <label class="support__form-label" for="username">Name: </label>
-//                             <div class="support__form-input-container">
-//                                 <input type="text" class="support__form-input" id="username" name="username"
-//                                     placeholder="Name" required>
-//                             </div>
-//                         </fieldset>
-
-//                         <fieldset class="support__form-field">
-//                             <label class="support__form-label" for="email">Email: </label>
-//                             <div class="support__form-input-container">
-//                                 <input type="email" class="support__form-input" id="email" name="user-email"
-//                                     placeholder="Email" required>
-//                             </div>
-//                         </fieldset>
-
-//                         <textarea class="support__form-input support__form-textarea" name="user-message" id="message"
-//                             cols="41" rows="10" placeholder="Message" required></textarea>
-
-//                         <button type="submit" class="support__button">Submit</button>
-
-//                         <p class="support__link">You can also support us by emailing<a href="" class="link">
-//                                 Support@liquidmoni.com</a></p>
-//                     </section>
-//                 </form>
-//             </section>
-//         </div>")
-// }
--- backend-auth/create-user.js
+++ backend-auth/create-user.js
@@ -1,18 +1,16 @@
+
+
 import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "./login-user";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 const auth = getAuth();
-// import { getDatabase, ref, set } from "firebase/database";
-// import { signUpFormSubmitButton } from "../Js/forms";
-// const App1 = initializeApp(firebaseConfig);
-// const dbForNewUsers = getDatabase();
 const firebaseConfig = {
   apiKey: "AIzaSyBNVTxjCatZxSiZhc5HY8Gr64lBMlb9fiA",
   authDomain: "liquidmoni-project-3d474.firebaseapp.com",
   projectId: "liquidmoni-project-3d474",
   storageBucket: "liquidmoni-project-3d474.appspot.com",
   messagingSenderId: "305360626126",
-  appId: "1:305360626126:web:7f8889f18c97247a960b9f",
+  appId: "1:305360626126:web:7f8889f18c 97247a960b9f",
   measurementId: "G-0QLYZCMM04",
 };
 
@@ -33,23 +31,4 @@ signUpFormSubmit.addEventListener("submit", (e) => {
     .then((signedInUser) => {
       console.log(signedInUser);
     });
-  // checkEmailInput(signUpEmailInput, signUpPasswordInput);
-  // handleSignUp(signUpEmailInput, signUpPasswordInput);
 });
-
-// let handleSignUp = (emailInput, passwordInput) => {
-//   // authentication
-//   createUserWithEmailAndPassword(auth1, emailInput.value, passwordInput.value)
-//     .then((signInDetails) => {
-//       const newUser = signInDetails.user;
-
-//       // store the user's data into the database
-//       set(ref(dbForNewUsers, "users/" + user.uid), {
-//         emailAddress: emailInput.value,
-//       });
-//     })
-//     .catch((error) => {
-//       const errorCode = error.code;
-//       const errorMessage = error.message;
-//     });
-// };
